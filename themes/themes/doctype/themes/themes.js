ad3ee5e714c138d8b1142c58207e98f9c5e4bc6d
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-24 16:10:19 +0530

    changes

--- themes/themes/doctype/themes/themes.js
+++ themes/themes/doctype/themes/themes.js
@@ -4,5 +4,15 @@
 frappe.ui.form.on('Themes', {
 	refresh: function(frm) {
 
+	},
+	theme: function(frm){
+		console.log("Test");
+		var linkNode = document.getElementsByTagName('link');
+		for(var i = 0; i < linkNode.length; i++){
+			var link = linkNode[i];
+			if (link.href.indexOf('custom.css') > 0){
+				
+			}
+		}
 	}
 });
